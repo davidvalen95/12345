105a0f7af6a24363c5162c42a2232a20dced7b24
David Valentino <davidvalen95@gmail.com>
2017-08-31 10:49:19 +0700

    error

--- resources/views/layouts/alert.blade.php
+++ resources/views/layouts/alert.blade.php
@@ -12,10 +12,23 @@
     </div>
 @endIf
 @if ($danger)
+
         <div class="alert alert-danger alert-dismissible">
                         <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
                         <h4><i class="icon fa fa-info"></i> Ooopppss...</h4>
                         {!!$danger!!}
           </div>
       {{-- type --}}
+@endif
+
+@if ($errors->any())
+    <div class="alert alert-danger alert-dismissible">
+        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+        <h4><i class="icon fa fa-info"></i> Ooopppss...</h4>
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
 @endif
