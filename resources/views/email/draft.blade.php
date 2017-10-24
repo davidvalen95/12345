fe8b7ed4040a8c598d2a205f459039fa2aa670d1
David Valentino <davidvalen95@gmail.com>
2017-10-24 09:58:06 +0700

    yes

--- resources/views/email/draft.blade.php
+++ resources/views/email/draft.blade.php
@@ -46,6 +46,9 @@
         </div>
     </section>
 
+    @php
+        define("name",'name');
+    @endphp
     <div style='text-align:center'>
         <p style='font-size:18px;'>
             Hallo <b>David</b> !!
@@ -59,6 +62,50 @@
         <p style='14px'>
             Ada fitur baru dari gbzworshipper.com yaitu baca alpet ga pake ribet. lgsg buka dan baca ayat2 untuk hari ini.. klik link di bawah ini ya
         </p>
+        <a href='http://gbzworshipper.com/alpet' style='margin-top:16px; display: inline-block; text-align:center;background-color:#7D56A9;color:white; padding:10px'>Alpet hari ini</a>
+
+    </div>
+
+
+
+
+
+
+
+    <div style='text-align:center'>
+        <p style='font-size:18px;'>
+            Hallo GBZers, <b>{{name}}</b>
+        </p>
+        <p style='font-size:15px;color:#9f9f9f;'>
+            Ada fitur baru lagi
+        </p>
+        <p style='14px'>
+            Kamu sekarang bisa nge-save ayat-ayat favorit waktu lagi baca alpet. Jadi ayat itu akan disimpen di idmu. Kamu bisa melihat ayat favoritmu kapanpun
+        </p>
+        <p style='14px'>
+            Ayat akan dikelompokan sesuai <i><b>Tag</b></i> jadi memudahkan untuk di baca ulang
+        </p>
+
+        <div style='display:block;margin:12px auto; max-width:350px;'>
+            <p>
+                Langkah awal klik hati disebelah ayat
+            </p>
+            <img style='width: 100%;' src='http://gbzworshipper.com/images/alpet/1.png'/>
+            <p>
+                Langkah dua, isi tag untuk tagging ayat, atau bisa klik tombol tag yang tersedia
+            </p>
+            <img style='width: 100%;' src='http://gbzworshipper.com/images/alpet/2.png'/>
+            <p>
+                Langkah tiga, buka ayat favoritmu di menu
+            </p>
+            <img style='width: 100%;' src='http://gbzworshipper.com/images/alpet/3.png'/>
+            <p>
+                Langkah empat, Ayat di kelompokan sesuai tag untuk memudahkan membaca
+            </p>
+            <img style='width: 100%;' src='http://gbzworshipper.com/images/alpet/4.png'/>
+
+        </div>
+
         <a href='http://gbzworshipper.com/alpet' style='margin-top:16px; display: inline-block; text-align:center;background-color:#7D56A9;color:white; padding:10px'>Alpet hari ini</a>
 
     </div>
